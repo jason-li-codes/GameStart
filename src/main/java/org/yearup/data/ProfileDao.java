ad3dceff07c423af8fcbd4fe017c961db1dffd0e
lijas <lijason991@gmail.com>
2025-12-18 21:43:14 -0500

    added new parent class methods

--- src/main/java/org/yearup/data/ProfileDao.java
+++ src/main/java/org/yearup/data/ProfileDao.java
@@ -3,7 +3,9 @@ package org.yearup.data;
 
 import org.yearup.models.Profile;
 
-public interface ProfileDao
-{
+public interface ProfileDao {
+
     Profile create(Profile profile);
+    Profile update(int userId, Profile profile);
+    Profile getProfileByUserId(int id);
 }
